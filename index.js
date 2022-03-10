23d9e03cdb8d8eef1d83af0a37a5d39c7793882b
Raghvendra Verma <raghvendraverma681@gmail.com>
2022-03-10 22:54:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+//Detecting Button Press
+var numberOfDrumButtons=document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfDrumButtons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+       // alert("i got clicked!");
+      // var audio=new Audio("sounds/tom-1.mp3");
+       //audio.play();
+       //console.log(this.style.color="white");
+       var buttonInnerHTML=this.innerHTML;
+       makeSound(buttonInnerHTML);
+       buttonAnimation(buttonInnerHTML);
+    });
+            
+}
+
+//Detecting Keyboard Press
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+
+    switch(key){
+        case "w":
+           var tom1=new Audio("sounds/tom-1.mp3");
+           tom1.play();
+        case "a":
+           var tom2=new Audio("sounds/tom-2.mp3");
+           tom2.play();
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        case "d":
+           var tom4=new Audio("sounds/tom-4.mp3");
+           tom4.play();
+        case "j":
+           var snare=new Audio("sounds/snare.mp3");
+           snare.play();
+        case "k":
+           var crash=new Audio("sounds/crash.mp3");
+           crash.play();
+        case "l":
+           var kick=new Audio("sounds/kick-bass.mp3");
+           kick.play();
+        default:
+            console.log(buttonInnerHTML);    
+             
+   }
+
+}
+function buttonAnimation(currentKey){
+   var activeButton = document.querySelector("." + currentKey);
+   activeButton.classList.add("pressed");
+   setTimeout(function(){
+      activeButton.classList.remove("pressed");
+   },100);
+
+}
